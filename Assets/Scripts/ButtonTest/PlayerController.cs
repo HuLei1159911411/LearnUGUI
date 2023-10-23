cbad8f9d7805497fdfbf7021a308bd137c160809
HuLei <1159911411@qq.com>
2023-10-23 11:51:43 +0800

    对上一次提交内容 未进行版本管理的文件进行版本管理

---
+++ Assets/Scripts/ButtonTest/PlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject bulletPrefab;
+    public void Fire()
+    {
+        if (bulletPrefab == null)
+            bulletPrefab = Resources.Load<GameObject>("BulletPrefab");
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
