ea48c718d7f1843e28e238520ac1e62cd8d0bc5e
HuLei <1159911411@qq.com>
2023-10-30 18:02:08 +0800

    Test38 异型按钮 1. 添加G7图片png资源 2. 通过添加子对象来调整点击范围制作异型按钮 3. 创建Test38脚本，在脚本中修改点击响应图片alpha阈值来调整

---
+++ Assets/Scripts/Test38/Test38.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test38 : MonoBehaviour
+{
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        image.alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
