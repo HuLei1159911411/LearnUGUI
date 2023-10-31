3fa705e992cea6710c70e9303e21f631854b9545
HuLei <1159911411@qq.com>
2023-10-31 16:17:01 +0800

    Test39自动布局 1. 修改之前ScrollView中BagPanel脚本中控制item排列的代码注释，为BagPanel中ScrollView中Viewport中Content添加GirdLayoutGroup和ContentSizeFitter组件并修改属性来达到自动布局及自动扩充区域

--- Assets/Scripts/ScrollViewTest/BagPanel.cs
+++ Assets/Scripts/ScrollViewTest/BagPanel.cs
@@ -9,8 +9,10 @@ public class BagPanel : MonoBehaviour
     public Button buttonCloseBagPanel;
     public ScrollRect scrollView;
     public int itemNum = 40;
-    public Vector2 offset = new Vector2(30, 30);
-    public Vector2 itemSize;
+    // 图片与范围偏移量，图片之间的偏移量为此偏移量减10,Test39之后使用GridLayoutGroup和ContentSizeFitter来进行布局管理
+    // public Vector2 offset = new Vector2(30, 30);
+    // 图片的尺寸大小，Test39之后使用GridLayoutGroup和ContentSizeFitter来进行布局管理
+    // public Vector2 itemSize;
     
     private void Start()
     {
@@ -21,10 +23,12 @@ public class BagPanel : MonoBehaviour
         for (int i = 0; i < itemNum; i++)
         {
             GameObject item = Instantiate(Resources.Load<GameObject>("Item"), scrollView.content);
-            item.transform.localPosition = new Vector3(i % 4 * (itemSize.x + offset.x - 10) + offset.x, -(i / 4 * (itemSize.x + offset.y - 10) + offset.y), 0);
+            // 设置item的位置,Test39之后改用GridLayoutGroup组件控制排列
+            // item.transform.localPosition = new Vector3(i % 4 * (itemSize.x + offset.x - 10) + offset.x, -(i / 4 * (itemSize.x + offset.y - 10) + offset.y), 0);
         }
-
-        scrollView.content.sizeDelta = new Vector2(0, Mathf.CeilToInt(itemNum / 4f) * (itemSize.x + offset.y - 10) + offset.y);
+        
+        // 设置content大小，Test39之后改用ContentSizeFitter控制大小变换
+        // scrollView.content.sizeDelta = new Vector2(0, Mathf.CeilToInt(itemNum / 4f) * (itemSize.x + offset.y - 10) + offset.y);
         
         gameObject.SetActive(false);
     }
